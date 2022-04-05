75cca8f9702137a211fc2376a0507406ebe7837c
subham sinha <subham.sinha41@gmail.com>
2022-04-05 12:44:31 +0530

    code convention added

--- src/components/Gmail/MainEvent.jsx
+++ src/components/Gmail/MainEvent.jsx
@@ -4,7 +4,6 @@ import { send } from 'emailjs-com'
 // this is the handler for the email transaction
 function MainEvent() {
   const [sender_name, set_sender_name] = useState('');
-  const [image, set_image] = useState('');
   const [sender_email, set_sender_email] = useState('');
   const [sender_message, set_sender_message] = useState('');
 
@@ -20,21 +19,16 @@ function MainEvent() {
   {
     set_sender_message(e.target.value)
   }
-  const handleImage = () =>
-  {
-    return(
-      <div>
-        <img src="https://i.ibb.co/54XSvtx/DT-banner.jpg" alt="image"></img>
-      </div>
-    );
-  }
   const sendMail = (e) =>
   {
     e.preventDefault();
     send(
-      'service_5ucez7e',
+      // gmail service id
+      'service_5ucez7e', 
+      // template id
       'template_96pvcem',
       {sender_name, sender_email, sender_message},
+      // user_id
       'htNbILA46JBpjwWnB'
     )
     .then((response) => {
@@ -53,7 +47,6 @@ function MainEvent() {
       placeholder="your name"></input><br />
       <input type="email" name="sender_email" className="form-control" value={sender_email} onChange = {handleEmail} required
       placeholder="your email"></input><br />
-      <p style = {{ display: "none"}} value = {image} onChange = {handleImage}></p>
       <input type="text" name="sender_message" value={sender_message} className="form-control" onChange = {handleMessage} required
       placeholder="your message"></input><br />
       {/* <input type="submit" name="submit" value={submit}></input> */}
